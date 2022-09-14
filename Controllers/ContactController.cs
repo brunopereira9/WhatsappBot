c795a418f809a2b3434817b7e237feda7ad88a05
Bruno Pereira <brunno.pereira7@gmail.com>
2022-09-14 00:42:59 -0300

    namespace adjustment

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WhatsappBot.Dto;
 using WhatsappBot.Entities;
-using WhatsappBot.Interfaces;
+using WhatsappBot.Repositories.Interfaces;
 
 namespace WhatsappBot.Controllers
 {
